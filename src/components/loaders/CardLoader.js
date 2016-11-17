18ebcc8ce36d35b46ebce7fcc98d054af4b4d799
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-11-17 16:28:58 +0700

    :lipstick: create loader

---
+++ src/components/loaders/CardLoader.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const CardLoader = () => (
+  <div className="ui card">
+    <div className="content">
+      <div className="background-masker short"></div>
+    </div>
+    <div className="image">
+      <div className="background-masker picture"></div>
+    </div>
+    <div className="content">
+      <p className="background-masker"></p>
+      <div className="meta">
+        <div className="background-masker"></div>
+      </div>
+      <div className="description">
+        <div className="background-masker"></div>
+      </div>
+    </div>
+    <div className="extra content">
+      <div className="background-masker short"></div>
+    </div>
+  </div>
+);
+
+export default CardLoader;
