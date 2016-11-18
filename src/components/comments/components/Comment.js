c944a44f22c85ca9d6eff35bbc17adbe4c28f17a
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-11-18 17:48:27 +0700

    :construction: add reply form

--- src/components/comments/components/Comment.js
+++ src/components/comments/components/Comment.js
@@ -1,26 +1,57 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
+import CommentReplyForm from './CommentReplyForm';
 
-const Comment = props => (
-  <div className="comment">
-    <a className="avatar">
-      <img src={props.avatarUrl} alt="User avatar" />
-    </a>
-    <div className="content">
-      <a className="author">{props.name}</a>
-      <div className="metadata">
-        <span className="date">{props.commentTime}</span>
+class Comment extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showReplyForm: false,
+    };
+
+    this.toggleReplyForm = this.toggleReplyForm.bind(this);
+  }
+
+  toggleReplyForm() {
+    this.setState({ showReplyForm: !this.state.showReplyForm });
+  }
+
+  render() {
+    const props = this.props;
+
+    return (
+      <div className="comment">
+        <a className="avatar">
+          <img src={props.avatarUrl} alt="User avatar" />
+        </a>
+        <div className="content">
+          <a className="author">{props.name}</a>
+          <div className="metadata">
+            <span className="date">{props.commentTime}</span>
+          </div>
+          <div className="text">
+            {props.commentContent}
+          </div>
+          <div className="actions">
+            <a
+              href="/"
+              className="reply"
+              rel="nofollow"
+              onClick={evt => {
+                evt.preventDefault();
+                this.toggleReplyForm();
+              }}
+            >Reply</a>
+          </div>
+        </div>
+        <div className="comments">
+          {/* Nesting comments */}
+          {props.children}
+          {this.state.showReplyForm && <CommentReplyForm />}
+        </div>
       </div>
-      <div className="text">
-        {props.commentContent}
-      </div>
-      <div className="actions">
-        <a href="#" className="reply">Reply</a>
-      </div>
-    </div>
-    {/* Nesting comments */}
-    {props.children}
-  </div>
-);
+    );
+  }
+}
 
 Comment.propTypes = {
   avatarUrl: PropTypes.string,
---
+++ src/components/comments/components/CommentReplyForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CommentReplyForm = () => (
+  <form className="ui reply form">
+    <div className="ui fluid action input">
+      <input type="text" />
+      <div className="ui button teal">Reply</div>
+    </div>
+  </form>
+);
+
+export default CommentReplyForm;
