9c24960c3f42c08b94a4dd7d55848d15486c0e88
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2016-11-18 16:28:43 +0700

    create rating component

--- src/components/cards/components/CardContent.js
+++ src/components/cards/components/CardContent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import CardRating from './CardRating';
 
 const CardContent = ({
   id,
@@ -7,16 +8,11 @@ const CardContent = ({
   printLength,
   author,
   publicationDate,
+  rating,
 }) => (
   <div className="content">
     <Link to={`post/${id}`} className="header">{title}</Link>
-    <div className="meta">
-      <i className="star icon"></i>
-      <i className="star icon"></i>
-      <i className="star icon"></i>
-      <i className="star icon"></i>
-      <i className="star empty icon"></i>
-    </div>
+    <CardRating rating={rating} />
     <div className="description">
       <div className="ui list">
         <div className="item"><strong>Print Length</strong>: {printLength} pages</div>
